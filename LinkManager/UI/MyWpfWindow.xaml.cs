5b7c6c495750c8383f9c5e908aa78842322feff8
WestPrime <westprime512@gmail.com>
2025-04-06 23:10:50 +0500

    +

---
+++ LinkManager/UI/MyWpfWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿// MyWpfWindow.xaml.cs
+using System.Windows;
+
+namespace YourNamespace
+{
+    public partial class MyWpfWindow : Window
+    {
+        public MyWpfWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button clicked!");
+        }
+    }
+}

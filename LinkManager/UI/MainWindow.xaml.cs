4bf346d3a07bf47f287d7157d0453b4d0f282c58
plohijie <katsuko.rize@gmail.com>
2025-05-09 03:39:35 +0500

    checkboxes update

--- LinkManager/UI/MainWindow.xaml.cs
+++ LinkManager/UI/MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Controls;
 using System.IO;
 using System.Windows.Forms;
 using MessageBox = System.Windows.MessageBox;
+using System.ComponentModel;
+using System.Linq;
 
 namespace LinkManager
 {
@@ -16,12 +18,12 @@ namespace LinkManager
     {
         struct Options // RomodanEA: советую привязывать функции к этой штуке
         {
-            string       ChosenFilePath;
-            bool         SearchInSubdirectories;
+            string ChosenFilePath;
+            bool SearchInSubdirectories;
             PositionType PositionType;
-            bool         SavePositions;
-            bool         TransferCoordinates;
-            bool         ShowOnlyProblems;
+            bool SavePositions;
+            bool TransferCoordinates;
+            bool ShowOnlyProblems;
         }
 
         public enum PositionType
@@ -50,6 +52,41 @@ namespace LinkManager
         public ObservableCollection<FileItem> FileItems { get; set; }
         public ObservableCollection<LinkItem> LinkItems { get; set; }
 
+        // Код для чекбоксов
+        private bool _isAllFilesSelected;
+        public bool? IsAllFilesSelected
+        {
+            get => _isAllFilesSelected;
+            set
+            {
+                if (_isAllFilesSelected != value)
+                {
+                    _isAllFilesSelected = value ?? false;
+                    OnPropertyChanged(nameof(IsAllFilesSelected));
+                    UpdateFilesSelection(_isAllFilesSelected);
+                }
+            }
+        }
+
+        private bool _isAllLinksSelected;
+        public bool? IsAllLinksSelected
+        {
+            get => _isAllLinksSelected;
+            set
+            {
+                if (_isAllLinksSelected != value)
+                {
+                    _isAllLinksSelected = value ?? false;
+                    OnPropertyChanged(nameof(IsAllLinksSelected));
+                    UpdateLinksSelection(_isAllLinksSelected);
+                }
+            }
+        }
+
+        public ICommand ToggleAllFilesCommand { get; }
+        public ICommand ToggleAllLinksCommand { get; }
+
+
         public ICommand ActionCommand { get; }
         public Document doc;
         public List<RevitLinkType> LinkTypes;
@@ -59,6 +96,7 @@ namespace LinkManager
             doc = document;
             LinkTypes = Link_Methods.GetLinks(doc);
             InitializeComponent();
+            DataContext = this;
             string dirName = NameSearchField.Text;
             paths = Directory.GetFiles(dirName, "*.rvt", SearchOption.AllDirectories);
 
@@ -68,10 +106,18 @@ namespace LinkManager
             FileItems = new ObservableCollection<FileItem>();
             LinkItems = new ObservableCollection<LinkItem>();
 
+            // Команда для чекбокса "Все"
+            ToggleAllFilesCommand = new RelayCommand(param =>
+            {
+                IsAllFilesSelected = !IsAllFilesSelected;
+            });
+            ToggleAllLinksCommand = new RelayCommand(param =>
+            {
+                IsAllLinksSelected = !IsAllLinksSelected;
+            });
+
             // Заполнение текущими данными
             UpdateData();
-
-            DataContext = this;
         }
         private void UpdateData() // Обновление данных
         {
@@ -225,72 +271,178 @@ namespace LinkManager
 
         }
 
-        private void AllCheckBox_Checked(object sender, RoutedEventArgs e)
+        private void UpdateFilesSelection(bool isSelected)
         {
-            if (LinksListView.ItemsSource is System.Collections.IEnumerable items)
+            foreach (var item in FileItems)
             {
-                foreach (var item in items)
+                item.IsSelected = isSelected;
+            }
+        }
+
+        private void UpdateLinksSelection(bool isSelected)
+        {
+            if (LinkItems == null)
+            {
+                LinkItems = new ObservableCollection<LinkItem>();
+            }
+            foreach (var item in LinkItems)
+            {
+                item.IsSelected = isSelected;
+            }
+        }
+
+        private void FileItem_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(FileItem.IsSelected))
+            {
+                bool allSelected = FileItems.All(item => item.IsSelected);
+                IsAllFilesSelected = allSelected ? true : false;
+            }
+        }
+
+        private void LinkItem_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(LinkItem.IsSelected))
+            {
+                bool allSelected = LinkItems.All(item => item.IsSelected);
+                IsAllLinksSelected = allSelected ? true : false;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is System.Windows.Controls.ListViewItem listViewItem && listViewItem.DataContext is INotifyPropertyChanged item)
+            {
+                if (item is FileItem fileItem)
                 {
-                    if (item is LinkItem linkItem) // Замените YourItemType на тип ваших элементов
-                    {
-                        linkItem.IsSelected = true;
-                    }
+                    fileItem.IsSelected = !fileItem.IsSelected;
+                }
+                else if (item is LinkItem linkItem)
+                {
+                    linkItem.IsSelected = !linkItem.IsSelected;
                 }
             }
         }
 
-        private void AllCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            if (LinksListView.ItemsSource is System.Collections.IEnumerable items)
+            if (sender is System.Windows.Controls.CheckBox checkBox && checkBox.DataContext is INotifyPropertyChanged item)
             {
-                foreach (var item in items)
+                if (item is FileItem fileItem)
                 {
-                    if (item is LinkItem linkItem) // Замените YourItemType на тип ваших элементов
-                    {
-                        linkItem.IsSelected = false;
-                    }
+                    fileItem.IsSelected = true;
+                }
+                else if (item is LinkItem linkItem)
+                {
+                    linkItem.IsSelected = true;
+                }
+            }
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (sender is System.Windows.Controls.CheckBox checkBox && checkBox.DataContext is INotifyPropertyChanged item)
+            {
+                if (item is FileItem fileItem)
+                {
+                    fileItem.IsSelected = false;
+                }
+                else if (item is LinkItem linkItem)
+                {
+                    linkItem.IsSelected = false;
                 }
             }
         }
     }
 
-    public class FileItem
-    {
-        public string FileName { get; set; }
-        public string Path { get; set; }
-    }
+}
 
-    public class LinkItem
-    {
-        public string LinkName { get; set; }
-        public string Status { get; set; }
-        public Brush StatusColor { get; set; }
-        public string ActionText { get; set; }
-        public Brush ActionColor { get; set; }
+public class FileItem : INotifyPropertyChanged
+{
+    private bool _isSelected;
+    public string FileName { get; set; }
+    public string Path { get; set; }
 
-        public RevitLinkType ToRevitLinkType
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set
         {
-            get
+            if (_isSelected != value)
             {
-                return new RevitLinkType()
-                {
-                    // Указать параметры возвращаемого
-                };
+                _isSelected = value;
+                OnPropertyChanged(nameof(IsSelected));
             }
         }
     }
 
-    // Реализация RelayCommand
-    public class RelayCommand : ICommand
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
     {
-        private readonly Action<object> _execute;
-
-        public RelayCommand(Action<object> execute) => _execute = execute;
-
-        public bool CanExecute(object parameter) => true;
-
-        public void Execute(object parameter) => _execute(parameter);
-
-        public event EventHandler CanExecuteChanged;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+}
+
+public class LinkItem : INotifyPropertyChanged
+{
+    private bool _isSelected;
+
+    public string LinkName { get; set; }
+    public string Status { get; set; }
+    public Brush StatusColor { get; set; }
+    public string ActionText { get; set; }
+    public Brush ActionColor { get; set; }
+
+    //public RevitLinkType ToRevitLinkType
+    //{
+    //    get
+    //    {
+    //        return new RevitLinkType()
+    //        {
+    //            // Указать параметры возвращаемого
+    //        };
+    //    }
+    //}
+
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set
+        {
+            if (_isSelected != value)
+            {
+                _isSelected = value;
+                OnPropertyChanged(nameof(IsSelected));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+// Реализация RelayCommand
+public class RelayCommand : ICommand
+{
+    private readonly Action<object> _execute;
+
+    public RelayCommand(Action<object> execute) => _execute = execute;
+
+    public bool CanExecute(object parameter) => true;
+
+    public void Execute(object parameter) => _execute(parameter);
+
+    public event EventHandler CanExecuteChanged;
 }
